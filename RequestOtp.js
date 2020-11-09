fb5f0a15776255fe8ec6ad384babf1e616e05b2d
Arjun Adobe <51747982+arjunadobe@users.noreply.github.com>
2020-11-10 01:51:17 +0530

    Update RequestOtp.js

--- RequestOtp.js
+++ RequestOtp.js
@@ -1,42 +1,56 @@
-import React from 'react';
-import { GET_OTP } from '../queries/sendOtp.gql';
-import { useLazyQuery } from '@apollo/react-hooks';
-import defaultClasses from './login.scss';
-import Button from '@magento/venia-ui/lib/components/Button/button';
+import React, { useEffect, useRef, useCallback } from "react";
+import { GET_OTP } from "../queries/sendOtp.gql";
+import { useLazyQuery } from "@apollo/react-hooks";
+import defaultClasses from "./login.scss";
+import Button from "@magento/venia-ui/lib/components/Button/button";
+import { useFieldState } from "informed";
 
-const RequestOtp = (props) => {
-	// Fetch the data using apollo react hooks
-	const validateOtp = () => {
-		if(props.telephone != null) {
-			query()
-		} else {
-			let phone = document.getElementById("enterPhone");
-			phone.classList.remove(defaultClasses.hide);
-			phone.classList.add(defaultClasses.requirePhonenumber);
-		}
-	  }
-	const [query,  { called, loading, data }] = useLazyQuery(GET_OTP, {
-			 onCompleted: data => {
-				if(data){
-					if(data.sendOTPtoPhone.response == "true") {
-						console.log(data);
-						let btn = document.getElementById("requestOtpBtn");
-						btn.classList.add(defaultClasses.hide);
-						let otp = document.getElementById("enterOtp");
-						otp.classList.remove(defaultClasses.hide);
-					} else {
-						let validphoneChcek = document.getElementById("enterPhoneValid");
-						validphoneChcek.classList.remove(defaultClasses.hide);
-						validphoneChcek.classList.add(defaultClasses.requirePhonenumber);
-					}
-					return (<Button id="btn" className={defaultClasses.requestOtpBtn} onClick={() => validateOtp()}>{'Request OTP'}</Button>);
-				}		
-			 },
-			fetchPolicy: "network-only",
-			variables: {telephone: props.telephone}
-		});
+const RequestOtp = () => {
+    const phoneVals = useFieldState("phone").value;
+    const [queryOtp, { data, error, loading }] = useLazyQuery(GET_OTP, {
+        fetchPolicy: "network-only",
+        onCompleted: (data) => {
+            if (data.sendOTPtoPhone.response == "true") {
+                let btn = document.getElementById("requestOtpBtn");
+                btn.classList.add(defaultClasses.hide);
+                let otp = document.getElementById("enterOtp");
+                otp.classList.remove(defaultClasses.hide);
+                const phone = document.getElementById("enterPhoneValid");
+                phone.classList.add(defaultClasses.hide);
+            } else {
+                let validphoneChcek = document.getElementById(
+                    "enterPhoneValid"
+                );
+                validphoneChcek.classList.remove(defaultClasses.hide);
+                validphoneChcek.classList.add(
+                    defaultClasses.requirePhonenumber
+                );
+                let otp = document.getElementById("enterOtp");
+                otp.classList.add(defaultClasses.hide);
+            }
+        },
+    });
+    const validate = useCallback(() => {
+        if (phoneVals != null) {
+            queryOtp({
+                variables: { telephone: phoneVals },
+            });
+        } else {
+            const phone = document.getElementById("enterPhoneValid");
+            phone.classList.remove(defaultClasses.hide);
+            phone.classList.add(defaultClasses.requirePhonenumber);
+        }
+    }, [phoneVals, queryOtp]);
 
-		return (<Button id="btn" className={defaultClasses.requestOtpBtn} onClick={() => validateOtp()}>{'Request OTP'}</Button>);
-}
+    return (
+        <Button
+            className={defaultClasses.requestOtpBtn}
+            disabled={loading}
+            onClick={validate}
+        >
+            {"Request OTP"}
+        </Button>
+    );
+};
 
 export default RequestOtp;
