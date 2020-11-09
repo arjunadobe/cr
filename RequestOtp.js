3d672b90e0eae5f801c15696e90c83d013237692
Arjun Adobe <51747982+arjunadobe@users.noreply.github.com>
2020-11-09 19:01:37 +0530

    Update RequestOtp.js

--- RequestOtp.js
+++ RequestOtp.js
@@ -6,21 +6,37 @@ import Button from '@magento/venia-ui/lib/components/Button/button';
 
 const RequestOtp = (props) => {
 	// Fetch the data using apollo react hooks
-	const [sendOtp,  { called, loading, data }] = useLazyQuery(GET_OTP, {
+	const validateOtp = () => {
+		if(props.telephone != null) {
+			query()
+		} else {
+			let phone = document.getElementById("enterPhone");
+			phone.classList.remove(defaultClasses.hide);
+			phone.classList.add(defaultClasses.requirePhonenumber);
+		}
+	  }
+	const [query,  { called, loading, data }] = useLazyQuery(GET_OTP, {
+			 onCompleted: data => {
+				if(data){
+					if(data.sendOTPtoPhone.response == "true") {
+						console.log(data);
+						let btn = document.getElementById("requestOtpBtn");
+						btn.classList.add(defaultClasses.hide);
+						let otp = document.getElementById("enterOtp");
+						otp.classList.remove(defaultClasses.hide);
+					} else {
+						let validphoneChcek = document.getElementById("enterPhoneValid");
+						validphoneChcek.classList.remove(defaultClasses.hide);
+						validphoneChcek.classList.add(defaultClasses.requirePhonenumber);
+					}
+					return (<Button id="btn" className={defaultClasses.requestOtpBtn} onClick={() => validateOtp()}>{'Request OTP'}</Button>);
+				}		
+			 },
+			fetchPolicy: "network-only",
 			variables: {telephone: props.telephone}
 		});
-	if (called && loading) return <p>Loading ...</p>
-	if (!called) {
-		return (<Button className={defaultClasses.requestOtpBtn} onClick={() => sendOtp()}>{'Request OTP'}</Button>);
-	}
-	if(data){
-		console.log(data);
-		let btn = document.getElementById("requestOtpBtn");
-		btn.classList.add(defaultClasses.hide);
-		let otp = document.getElementById("enterOtp");
-		otp.classList.remove(defaultClasses.hide);
-		return '';
-	}
+
+		return (<Button id="btn" className={defaultClasses.requestOtpBtn} onClick={() => validateOtp()}>{'Request OTP'}</Button>);
 }
 
 export default RequestOtp;
